b3730211deaed3bfc8e8a82c10858597f7d306e0
Tim Zerrell <tim.zerrell@risczero.com>
2024-11-04 11:55:35 -0800

    Clean up comments / error message

--- src/algorithms/rsa.rs
+++ src/algorithms/rsa.rs
@@ -26,10 +26,10 @@ pub fn rsa_encrypt<K: PublicKeyParts>(key: &K, m: &BigUint) -> Result<BigUint> {
     #[cfg(all(target_os = "zkvm", target_arch = "riscv32"))]
     {
         // If we're in the RISC Zero zkVM, try to use its RSA accelerator circuit
-        env::log("[TODO] `rsa_encrypt` start");
         if *key.e() == BigUint::new(vec!{65537}) {
-            // TODO: clean up error escalation
-            return Ok(risc0_circuit_bigint::rsa::modpow_65537(key.n(), m).expect("TODO"));
+            // TODO: Could escalate errors a bit more nicely
+            // (e.g. to fall through to non-accelerated version if error is safe for that)
+            return Ok(risc0_circuit_bigint::rsa::modpow_65537(key.n(), m).expect("Unable to run RSA accelerator"));
         }
         // Fall through when the exponent does not match the accelerator
     }
